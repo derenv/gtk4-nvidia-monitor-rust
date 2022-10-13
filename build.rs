c870032939f295df0b4abe14739e927fca4800e8
Deren Vural <derenv@live.co.uk>
2022-10-13 18:43:45 +0100

    Updated Cargo.toml - Changed from gtk4 to libadwaita for build script dependancy
    Updated Build Script (build.rs)
    - Added comment headers
    - Switched to adwaita gio wrapper
    - Added print statement for debugging
    
    Signed-off-by: Deren Vural <derenv@live.co.uk>

--- build.rs
+++ build.rs
@@ -1,12 +1,50 @@
 // SPDX-FileCopyrightText: 2022 Deren Vural
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-use gtk::gio;
+/*
+ * Name:
+ * build.rs
+ *
+ * Description:
+ * Build script for Nvidia Gnome Extension (Rust)
+ *
+ * Made:
+ * 09/10/2022
+ *
+ * Made by:
+ * Deren Vural
+ *
+ * Notes:
+ * imports must be added as a dependancy for build scripts via:
+ *     `cargo add gtk4 --rename gtk --build`
+ * which updates Cargo.toml with:
+ *     [build-dependencies]
+ *     gtk = { version = "^0.4.8", package = "gtk4" }
+ */
 
+// Imports
+use adwaita::gio::compile_resources;
+
+/*
+ * Name:
+ * main
+ *
+ * Description:
+ * Runs pre-build
+ *
+ * Made:
+ * 09/10/2022
+ *
+ * Made by:
+ * Deren Vural
+ *
+ * Notes:
+ *
+ */
 fn main() {
     // UI
-    println!("compiling UI resources");
-    gio::compile_resources(
+    println!("..Compiling UI resources into `.gresource` file");
+    compile_resources(
         "src/resources",
         "src/resources/resources.gresource.xml",
         "nvidiaextensionrust.gresource",
